3adc09f7d8f80d361c6aee2a77193eb11bfc03f4
kfiddle <kenjfiddle@gmail.com>
2021-07-06 22:39:03 -0400

    contact form email

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,17 +1,116 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useReducer } from "react";
 
-import styles from "./Contact.module.css";
+import emailjs from "emailjs-com";
 
+import styles from "./ContactForm.module.css";
 import Wrapper from "../Wrapper";
+
+
+const userId = "user_ziX5oSLNJRahUxs9dz2xC";
+const serviceId = "service_whc7i1l";
+const templateId = "template_xhux42i";
+
+const initialFormState = { name: "", email: "", message: "" };
+
+const inputReducer = (state, action) => {
+  if (action.type === "name") {
+    return { ...state, name: action.name };
+  } else if (action.type === "email") {
+    return { ...state, email: action.email };
+  } else if (action.type === "message") {
+    return { ...state, message: action.message };
+  } else {
+    return initialFormState;
+  }
+};
+
 
 const ContactForm = (props) => {
   const [risen, setRisen] = useState(110);
   const [textGlow, setTextGlow] = useState(0);
 
+  const [emptySubmission, setEmptySubmission] = useState(false);
+  const [foneNumber, setfoneNumber] = useState("");
+
+
+  const [inputs, setInputDispatcher] = useReducer(
+    inputReducer,
+    initialFormState
+  );
+
+  const name = useRef();
+  const email = useRef();
+  const phoneNumber = useRef();
+  const message = useRef();
+
+  const typedName = (event) =>
+    setInputDispatcher({ type: "name", name: event.target.value });
+  const typedEmail = (event) =>
+    setInputDispatcher({ type: "email", email: event.target.value });
+  const typedMessage = (event) =>
+    setInputDispatcher({ type: "message", message: event.target.value });
+
   useEffect(() => {
     setTimeout(() => setRisen(10), 3000);
     setTimeout(() => setTextGlow(1.0), 4000);
   }, []);
+
+  const formatNumber = (event) => {
+    if (isNaN(event.nativeEvent.data) || event.target.value.length === 13) {
+      return;
+    }
+
+    if (
+      (event.target.value.length === 3 || event.target.value.length === 7) &&
+      !isNaN(event.nativeEvent.data)
+    ) {
+      setfoneNumber((previous) => event.target.value + "-");
+    } else {
+      setfoneNumber((previous) => event.target.value);
+    }
+  };
+
+  const submissionCheck = (event) => {
+    setEmptySubmission(false);
+  };
+
+  const checkForDelete = (event) => {
+    setEmptySubmission(false);
+    if (
+      event.code === "Backspace" &&
+      foneNumber[foneNumber.length - 1] === "-"
+    ) {
+      setfoneNumber((previous) => previous.slice(0, -1));
+    }
+  };
+
+  const submitInfo = () => {
+    let contactInfo = {
+      fullName: name.current.value,
+      phoneNumber: phoneNumber.current.value,
+      emailAddress: email.current.value,
+      message: message.current.value,
+    };
+
+    let empty = true;
+
+    for (let key in contactInfo) {
+      if (contactInfo[key].length > 0) {
+        empty = false;
+      }
+    }
+
+    if (empty === true) {
+      setEmptySubmission(true);
+      return;
+    }
+
+    emailjs.send(serviceId, templateId, contactInfo, userId);
+    console.log(serviceId, templateId, contactInfo, userId);
+
+    setfoneNumber("");
+    setInputDispatcher({ type: "reset" });
+  };
 
   return (
     <Wrapper>
@@ -29,18 +128,42 @@ const ContactForm = (props) => {
 
           <div className={styles.contact_form}>
             <div className={styles.left_column}>
-              <input placeholder={"Name"}></input>
-              <input placeholder={"Email Address"}></input>
-              <input placeholder={"Phone"}></input>
+              <input
+                placeholder={"Name"}
+                ref={name}
+                onKeyDown={submissionCheck}
+                onChange={typedName}
+                value={inputs.name}
+              ></input>
+              <input
+                placeholder={"Email Address"}
+                ref={email}
+                onKeyDown={submissionCheck}
+                onChange={typedEmail}
+                value={inputs.email}
+              ></input>
+              <input
+                placeholder={"Phone"}
+                ref={phoneNumber}
+                onChange={formatNumber}
+                onKeyDown={checkForDelete}
+                value={foneNumber}
+              ></input>
             </div>
 
             <div className={styles.right_column}>
-              <textarea placeholder={"Message"}></textarea>
+              <textarea
+                placeholder={"Message"}
+                ref={message}
+                onKeyDown={submissionCheck}
+                onChange={typedMessage}
+                value={inputs.message}
+              ></textarea>
             </div>
           </div>
 
           <div className={styles.submit_button}>
-            <button style={{ opacity: textGlow }}>Send Message</button>
+            <button onClick={submitInfo} style={{ opacity: textGlow }}>Send Message</button>
           </div>
         </div>
       </div>
